d38d286441ba518f8022352688853cdaccdea307
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-11-11 23:58:33 -0500

    Add tests for Account

---
+++ frontend/__tests__/account.test.js
@@ -0,0 +1,65 @@
+import { mount } from 'enzyme';
+import { MockedProvider } from 'react-apollo/test-utils';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import Account, { USER_ORDERS_QUERY } from '../components/Account';
+import { fakeOrder } from '../lib/testUtils';
+import formatMoney from '../lib/formatMoney';
+
+const mocks = [
+  {
+    request: { query: USER_ORDERS_QUERY },
+    result: {
+      data: {
+        userOrders: [
+          fakeOrder(),
+          {
+            ...fakeOrder(),
+            id: 'ord124'
+          },
+          {
+            ...fakeOrder(),
+            id: 'ord125'
+          }
+        ]
+      }
+    }
+  }
+];
+
+const noOrdersMocks = [
+  {
+    request: { query: USER_ORDERS_QUERY },
+    result: {
+      data: {
+        userOrders: []
+      }
+    }
+  }
+];
+
+describe('<Account />', () => {
+  it('renders and matches the snapshot', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <Account />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(toJSON(wrapper.find('div[data-test="account"]'))).toMatchSnapshot();
+  });
+
+  it('renders a message when a user has no orders', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={noOrdersMocks}>
+        <Account />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.find('p').text()).toContain(
+      "Looks like you havn't ordered anything yet!"
+    );
+  });
+});
--- frontend/components/Account.js
+++ frontend/components/Account.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
-import {formatDistance} from 'date-fns';
+import { Query } from 'react-apollo';
+import { formatDistance } from 'date-fns';
 import styled from 'styled-components';
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
@@ -38,45 +38,58 @@ class Account extends Component {
   render() {
     return (
       <Query query={USER_ORDERS_QUERY}>
-        {({data, loading, error}) => {
-          const {userOrders} = data;
-          if (loading) return <Loading />
-          if(error) return <Error error={error} />
-          if (!userOrders.length) return <p>Looks like you havn't ordered anything yet!</p>
-          
+        {({ data, loading, error }) => {
+          const { userOrders } = data;
+          if (loading) return <Loading />;
+          if (error) return <Error error={error} />;
+          if (!userOrders.length)
+            return <p>Looks like you havn't ordered anything yet!</p>;
+
           return (
-            <div>
+            <div data-test='account'>
               <SubTitle>Your Account</SubTitle>
-              <h2>You have {userOrders.length} order{userOrders.length === 1 ? '' : 's'}</h2>
+              <h2>
+                You have {userOrders.length} order
+                {userOrders.length === 1 ? '' : 's'}
+              </h2>
               <OrdersGrid>
                 {userOrders.map(order => (
                   <OrderItem key={order.id}>
-                    <Link href={{
-                      pathname: '/order',
-                      query: { orderId: order.id }
-                    }}>
+                    <Link
+                      href={{
+                        pathname: '/order',
+                        query: { orderId: order.id }
+                      }}
+                    >
                       <a>
-                        <div className="order-meta">
-                          <p>{order.items.reduce((a, b) => a + b.quantity, 0)} Items</p>
-                          <p>{order.items.length} Product{order.items.length === 1 ? '' : 's'}</p>
+                        <div className='order-meta'>
+                          <p>
+                            {order.items.reduce((a, b) => a + b.quantity, 0)}{' '}
+                            Items
+                          </p>
+                          <p>
+                            {order.items.length} Product
+                            {order.items.length === 1 ? '' : 's'}
+                          </p>
                           <p>{formatDistance(order.createdAt, new Date())}</p>
                           <p>{formatMoney(order.total)}</p>
                         </div>
-                        <div className="images">
+                        <div className='images'>
                           {order.items.map(item => (
-                            <img 
-                              src={item.image} 
+                            <img
+                              src={item.image}
                               alt={item.title}
-                              key={item.id} />
+                              key={item.id}
+                            />
                           ))}
                         </div>
                       </a>
                     </Link>
                   </OrderItem>
                 ))}
-            </OrdersGrid>
+              </OrdersGrid>
             </div>
-          )
+          );
         }}
       </Query>
     );
@@ -84,3 +97,4 @@ class Account extends Component {
 }
 
 export default Account;
+export { USER_ORDERS_QUERY };
